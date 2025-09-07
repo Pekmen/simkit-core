de7ca99f7a89819d2905192bb5a750eb71266a10
Pekmen <milanpekovic89@gmail.com>
2025-09-07 17:50:58 +0200

    SK-1-004: added basic component storage

---
+++ src/core/ComponentStorage.ts
@@ -0,0 +1,36 @@
+import type { EntityId } from "./Entity";
+
+export class ComponetStorage<T> {
+  private components = new Map<EntityId, T>();
+
+  addComponent(entityId: EntityId, component: T): void {
+    this.components.set(entityId, component);
+  }
+
+  removeComponent(entityId: EntityId): boolean {
+    return this.components.delete(entityId);
+  }
+
+  getComponent(entityId: EntityId): T | undefined {
+    return this.components.get(entityId);
+  }
+
+  hasComponent(entityId: EntityId): boolean {
+    return this.components.has(entityId);
+  }
+
+  getAllComponents(): T[] {
+    return Array.from(this.components.values());
+  }
+  getAllEntities(): EntityId[] {
+    return Array.from(this.components.keys());
+  }
+
+  size(): number {
+    return this.components.size;
+  }
+
+  clear(): void {
+    this.components.clear();
+  }
+}
--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -1,1 +1,21 @@
 export type EntityId = number & { readonly __brand: unique symbol };
+
+export class EntityManager {
+  private nextId = 1 as EntityId;
+  private activeEntities: EntityId[] = [];
+
+  createEntity(): EntityId {
+    this.activeEntities.push(this.nextId++ as EntityId);
+    return this.nextId;
+  }
+
+  destroyEntity(entityId: EntityId): void {
+    this.activeEntities = this.activeEntities.filter((e: EntityId) => {
+      return e === entityId;
+    });
+  }
+
+  getAllActiveEntities(): EntityId[] {
+    return this.activeEntities;
+  }
+}
