d8bcf9071d12e6d72e4b014f125c367f7095569a
Pekmen <milanpekovic89@gmail.com>
2025-09-07 02:55:00 +0200

    SK-122: added basic Entity system

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,31 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    files: ["src/**/*.ts"],
+    plugins: { "@typescript-eslint": typescriptEslint },
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
   },
-  tseslint.configs.recommended,
-  tseslint.configs.recommended,
-  tseslint.configs.stylistic,
   {
     files: ["**/*.json"],
     plugins: { json },
---
+++ src/core/EntityManager.ts
@@ -0,0 +1,18 @@
+import type { Entity } from "./Entity";
+
+export class EntityManager {
+  private nextId = 0;
+  private entities: Entity[] = [];
+
+  create(): void {
+    this.entities.push(this.nextId++);
+  }
+
+  destroyEntity(entity: Entity): void {
+    this.entities = this.entities.filter((e: Entity) => e === entity);
+  }
+
+  getEntities(): Entity[] {
+    return this.entities;
+  }
+}
